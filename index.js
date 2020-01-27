002e4c1ca3a277e82c13f0f41ed84f6f2c1e5d3e
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-01-27 14:37:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function takeANumber (line, name) {
 
 function nowServing (line) {
 
-  if (line === undefined) {return 'There is nobody waiting to be served!'}
+  if (line.length === 0) {return 'There is nobody waiting to be served!'}
   else {
     let first = line[0];
     line.shift();
@@ -21,7 +21,7 @@ function nowServing (line) {
 
   function currentLine (line) {
 
-    if (line === undefined) {return `The line is currently empty.`}
+    if (line.length === 0) {return `The line is currently empty.`}
 
     let str  = `The line is currently: `;
 
