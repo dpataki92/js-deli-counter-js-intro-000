6a0d4fac8f38be0ec55ef5a458b233224f660866
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-01-27 14:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,28 @@ function takeANumber (line, name) {
   return `Welcome, ${name}. You are number ${length} in line.`
 
 }
+
+
+function nowServing (line) {
+
+  if (line === []) {return 'There is nobody waiting to be served!'}
+  else {
+    let first = line[0];
+    line.shift();
+    return first;
+  }
+
+  function currentLine (line) {
+
+    if (line === []) {return `The line is currently empty.`}
+
+    let str  = `The line is currently: `;
+
+    for (let i = 0; i < line.length; i++) {
+      
+      i === line.length-1 ? str += `${i}. ${line[i]}` : str += `${i}. ${line[i]}, `;  
+  }
+
+    return str;
+  
+}
